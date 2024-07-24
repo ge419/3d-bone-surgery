99973b8602b88c3e077eb971decf0c62873da5cb
Changmin Shin <cs521@duke.edu>
2024-07-25 00:50:04 +0900

    navigation example using about page

--- bone-client/app/page.tsx
+++ bone-client/app/page.tsx
@@ -5,7 +5,7 @@ import ResponsiveNav from "../components/Navigation/ResponsiveNav";
 const HomePage = () => {
   return (
     <>
-      <ResponsiveNav />
+      {/* <ResponsiveNav /> */}
       <div className="text-9xl text-blue-900">
         <Home />
       </div>
